854fad82986b7fd4454a8f83acdf0c1d8e0ddd6a
Kamalnath28 <kamalnath28052005@gmail.com>
2026-08-11 16:13:31 +0530

    Add responsive UI and issue report page

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/report")
+def report():
+    return render_template("report.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
