cf8e8f59b46ff118d3105415d77dd97c132911d1
Kamalnath28 <kamalnath28052005@gmail.com>
2026-08-19 18:25:49 +0530

    Connect Flask app to PostgreSQL

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
+import os
+import psycopg
+from dotenv import load_dotenv
+
+# Load values from .env
+load_dotenv()
 
 app = Flask(__name__)
+
+
+def get_db_connection():
+    connection = psycopg.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
+
+    return connection
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/report", methods=["GET", "POST"])
 def report():
@@ -16,9 +35,24 @@ def report():
         location = request.form["location"]
         description = request.form["description"]
 
-        print("Category:", category)
-        print("Location:", location)
-        print("Description:", description)
+        connection = get_db_connection()
+
+        cursor = connection.cursor()
+
+        cursor.execute(
+            """
+            INSERT INTO complaints (category, location, description)
+            VALUES (%s, %s, %s)
+            """,
+            (category, location, description)
+        )
+
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        print("Complaint saved successfully!")
 
     return render_template("report.html")
 
